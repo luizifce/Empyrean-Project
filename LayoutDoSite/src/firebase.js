76afc5d6d2ea72300b02d17571a0a08617e53370
Luiz Gabriel <bieel180360@gmail.com>
2023-06-21 03:15:53 -0300

    first commit

--- LayoutDoSite/src/firebase.js
+++ LayoutDoSite/src/firebase.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
+import firebase from "./firebase";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import {getFirestore} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -15,7 +17,7 @@ const firebaseConfig = {
   projectId: "empyreansupplements",
   storageBucket: "empyreansupplements.appspot.com",
   messagingSenderId: "677673543186",
-  appId: "1:677673543186:web:f956d1dca9b81aa3b19acb"
+  appId: "1:677673543186:web:f956d1dca9b81aa3b19acb",
 };
 
 // Initialize Firebase
@@ -23,5 +25,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-export {provider};
-export const db = getFirestore()
+export { provider };
+export const db = getFirestore();
+export const storage = getStorage(app);
---
+++ LayoutDoSite/src/produtos.js
@@ -0,0 +1,21 @@
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "./firebase";
+
+async function findAll() {
+  const querySnapshot = await getDocs(collection(db, "produtos"));
+  const docArray = [];
+
+  querySnapshot.forEach((doc) => {
+    const docData = doc.data();
+    docArray.push({
+      id: doc.id,
+      produtoNome: docData.produtoNome,
+      produtoDesc: docData.produtoDesc,
+      produtoPreco: docData.produtoPreco,
+    });
+  });
+
+  return docArray;
+}
+
+export default findAll;
